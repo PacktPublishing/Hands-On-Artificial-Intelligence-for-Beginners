fa3ba1d45411f2fe0c95e5b6d4fa66b62cef04e6
Patrick D. Smith <patricksmith1223@gmail.com>
2018-08-30 11:04:46 -0400

    Update app.py

--- chapter14/web_app/app.py
+++ chapter14/web_app/app.py
@@ -1,16 +1,13 @@
 from flask import Flask, jsonify
-from flask_restful import Api
 from flask_cors import CORS
 from predict import predict
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app)
 
 ## Hit the Predict API
 @app.route('/predict', methods=['GET'])
 def sendPredict():
-    '''Function for returning a list of node names based on type'''
     response = predict()
     print(response[0])
     return jsonify(response[0])
